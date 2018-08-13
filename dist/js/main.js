34fa33f9d36b7f64dfce9addf84b88caa781280c
Duhamel Guerrero <duhamelgm@gmail.com>
2018-08-13 13:46:10 -0400

    Update-1 13/08/2018

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,42 +3,10 @@ function headerButtonDisplay(){
     if (buttonHeaderState.className === "menu-nav-normal"){
         buttonHeaderState.className += " menu-nav-responsive";
 
-        var normalMenuHeight = 257;
-        var animatedMenuHeight = 53;
-        document.getElementById("navbar").style.height = 53;
-
-        var idMenu = setInterval(frameMenu, 17);
-
-        function frameMenu(){
-            if(normalMenuHeight <= animatedMenuHeight){
-                console.log(normalMenuHeight);
-                clearInterval(idMenu);
-            } else {
-                animatedMenuHeight += 10;
-                console.log(animatedMenuHeight);
-                document.getElementById("navbar").style.height = animatedMenuHeight + "px";
-            }
-        }
-
     } else {
 
-        var normalMenuHeight = 53;
-        var animatedMenuHeight = 257;
-        document.getElementById("navbar").style.height = 257;
-
-        var idMenu = setInterval(frameMenu, 17);
-
-        function frameMenu(){
-            if(normalMenuHeight >= animatedMenuHeight){
-                console.log(normalMenuHeight);
-                clearInterval(idMenu);
-                buttonHeaderState.className = "menu-nav-normal";
-            } else {
-                animatedMenuHeight -= 10;
-                console.log(animatedMenuHeight);
-                document.getElementById("navbar").style.height = animatedMenuHeight + "px";
-            }
-        }
+        buttonHeaderState.className = "menu-nav-normal";
     }
 
 }
+
